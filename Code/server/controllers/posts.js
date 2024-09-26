ec622cc63f1cdec7dda576c88bcde98ee5ba12a7
Devin Wijesiri <wijesiri2001@gmail.com>
2024-09-26 05:18:11 -0400

    styling for app.js doesn't work.I'm taking a break

--- Code/server/controllers/posts.js
+++ Code/server/controllers/posts.js
@@ -1,13 +1,25 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPost = (req, res) => {
+export const getPost = async (req, res) => {
     try {
-        
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
     } catch (error) {
-        
+        res.status(404).json({ message: error.message });  
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessages(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
